13c0519fe382c3f31be8226d3025a426610afe06
Ahmed Nour <ahmed.nor5445@gmail.com>
2023-09-11 18:42:40 -0700

    Hello C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print a qoute using write function write
+ *
+ * Return: 1 (not Success)
+ */
+
+int main(void)
+{
+	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.qu.59);
+	return (1);
+}
