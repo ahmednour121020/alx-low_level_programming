da86402efa53cd5a4ea21ce862a8357ae6690c61
Ahmed Nour <ahmed.nor5445@gmail.com>
2023-09-19 18:11:58 -0700

    C variables and if and else and while

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ *
+ * Description:  program that prints _putchar using
+ * putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
